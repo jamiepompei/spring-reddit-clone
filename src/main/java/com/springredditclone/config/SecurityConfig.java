0226c27b9eee831b0fee119292d608ce2e449ee5
nicolepompei <nicole.lynn.pompei@gmail.com>
2020-12-19 10:49:36 -0500

    security config class, override for configure method

---
+++ src/main/java/com/springredditclone/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.springredditclone.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//override configure method to disable csrf protetion for backend
+//csrf attacks occur when there are sessions and when we use cookies to authenticate session info
+//json web tokens for authorization so we can disable
+    @Override
+    public void configure(HttpSecurity httpSecurity){
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
